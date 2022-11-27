937ca79323612d41093fa4af44e533d6628f8465
naeem-bobada <gnaeem111@gmail.com>
2022-11-28 00:25:56 +0530

    set local host

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const ACTIONS = require('./actions');
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: process.env.FRONT_URL,
+        origin: "http://localhost:3000",
         methods: ['GET', 'POST'],
     },
 });
@@ -19,7 +19,7 @@ const io = require('socket.io')(server, {
 app.use(cookieParser());
 const corsOption = {
     credentials: true,
-    origin: [process.env.FRONT_URL],
+    origin: ["http://localhost:3000"],
 };
 app.use(cors(corsOption));
 app.use('/storage', express.static('storage'));
